fd041b10acd78342c3f29136b772fdf7dd944f3d
Neeraj Patel <114114241+neerajpatel1234@users.noreply.github.com>
2023-08-24 12:35:36 +1200

    Neeraj: added referance code for how to create a candle stick chart in java

---
+++ Referance_Code/zemlyakov_Candle.java
@@ -0,0 +1,50 @@
+package Referance_Code;//package com.soilprod.javafx.chart.candlestick.node;
+
+
+public class zemlyakov_Candle {
+    /*#
+        private Line highLowLine = new Line();
+        private Region bar = new Region();
+        private String seriesStyleClass;
+        private String dataStyleClass;
+        private boolean openAboveClose = true;
+
+        public Candle(String seriesStyleClass, String dataStyleClass) {
+            setAutoSizeChildren(false);
+            getChildren().addAll(highLowLine, bar);
+            this.seriesStyleClass = seriesStyleClass;
+            this.dataStyleClass = dataStyleClass;
+            updateStyleClasses();
+        }
+
+        public void setSeriesAndDataStyleClasses(String seriesStyleClass, String dataStyleClass) {
+            this.seriesStyleClass = seriesStyleClass;
+            this.dataStyleClass = dataStyleClass;
+            updateStyleClasses();
+        }
+
+        public void update(double closeOffset, double highOffset, double lowOffset, double candleWidth) {
+            openAboveClose = closeOffset > 0;
+            updateStyleClasses();
+            highLowLine.setStartY(highOffset);
+            highLowLine.setEndY(lowOffset);
+            if (candleWidth == -1) {
+                candleWidth = bar.prefWidth(-1);
+            }
+            if (openAboveClose) {
+                bar.resizeRelocate(-candleWidth / 2, 0, candleWidth, closeOffset);
+            } else {
+                bar.resizeRelocate(-candleWidth / 2, closeOffset, candleWidth, closeOffset * -1);
+            }
+        }
+
+        private void updateStyleClasses() {
+            getStyleClass().setAll("candlestick-candle", seriesStyleClass, dataStyleClass);
+            highLowLine.getStyleClass().setAll("candlestick-line", seriesStyleClass, dataStyleClass,
+                    openAboveClose ? "open-above-close" : "close-above-open");
+            bar.getStyleClass().setAll("candlestick-bar", seriesStyleClass, dataStyleClass,
+                    openAboveClose ? "open-above-close" : "close-above-open");
+        }
+
+     */
+    }
